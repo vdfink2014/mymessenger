4370ff037b49e0bf877a195e5af0952cb40da182
vdfink <vdfink@proton.me>
2026-02-22 20:00:53 +0300

    [[BIG SHOTS]]

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ with open('users.json') as f:
 def root():
     return render_template("index.html")
 
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
 @app.route("/register", methods=["POST"])
 def register():
     username = request.json["username"]
