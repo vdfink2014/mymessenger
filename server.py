da038f88c29fdce6a18e42ab2371ee1eaaf061fe
vdfink <vdfink@proton.me>
2026-02-22 19:03:39 +0300

    Doing the backend; todo: get a actual database

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
+
+with open('users.json') as f:
+    db = json.load(f)
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.json["username"]
+    publickey = request.json["publicKey"]
+    for user in db.keys():
+        if user == username:
+            return "Username already exists", 400
+    db[username] = {"publickey": publickey}
+    with open("users.json", "w") as f:
+        json.dump(db, f)
+    return "Register successful!", 200
+
+@app.route("/getpublickey", methods=["POST"])
+def getpublickey():
+    username = request.json["username"]
+    return json.dumps({"publicKey": db[username]["publickey"]}), 200
 
 app.run(port=8080, host="127.0.0.1")
 # hmm
